b4e0f41a261e4f26d2fbfb786082bbcbf08e052a
deeice <dheyse@hotmail.com>
2016-11-27 12:48:37 -0500

    Fixed PageUp, PageDn and root file device for Zipit openwrt.

--- src/def.h
+++ src/def.h
@@ -64,21 +64,21 @@
 #define PATH_DEFAULT        "/"
 #define FILE_SYSTEM         "/dev/mmcblk0p1"
 #elif defined(PLATFORM_ZIPIT)
-// Keys for GCW Zero
+// Keys for ZIPIT Z2 Openwrt (IZ2S uses PAGEUP, PAGEDOWN, and /dev/mmcblk0p1)
 #define MYKEY_UP            SDLK_UP
 #define MYKEY_RIGHT         SDLK_RIGHT
 #define MYKEY_DOWN          SDLK_DOWN
 #define MYKEY_LEFT          SDLK_LEFT
 #define MYKEY_SYSTEM        SDLK_ESCAPE
-#define MYKEY_PAGEUP        SDLK_PAGEUP
-#define MYKEY_PAGEDOWN      SDLK_PAGEDOWN
+#define MYKEY_PAGEUP        SDLK_HOME
+#define MYKEY_PAGEDOWN      SDLK_END
 #define MYKEY_OPEN          SDLK_RETURN
 #define MYKEY_PARENT        SDLK_BACKSPACE
 #define MYKEY_OPERATION     SDLK_TAB
 #define MYKEY_SELECT        SDLK_SPACE
 #define MYKEY_TRANSFER      SDLK_p
 #define PATH_DEFAULT        "/"
-#define FILE_SYSTEM         "/dev/mmcblk0p1"
+#define FILE_SYSTEM         "/dev/root"
 #else
 // Keys for PC keyboard
 #define MYKEY_UP            SDLK_UP
